0c167a4425f42f7bf8e6e2ddb40530aed38b40f6
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-07-17 14:05:20 +0000

    ファイルアップデート

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,16 @@
 <?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 use App\Book;
 use Illuminate\Http\Request;
 
@@ -22,3 +34,9 @@ Route::post('/books', function (Request $request) {
 Route::delete('/book/{book}', function (Book $book) {
     //
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
