6452959213a8a0d48055333d8aeb396efa44930b
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-07-21 02:50:24 +0000

    ルートのupdateメソッドをコントローラへ移動

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+//使うClassを宣言:自分で追加
+use App\Book;   //Bookモデルを使えるようにする
+use Validator;  //バリデーションを使えるようにする
+use Auth;       //認証モデルを使用する
+
+class BooksController extends Controller
+{
+    //更新
+    public function update(Request $request){
+        //バリデーション
+        $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'item_name' => 'required|min:3|max:255',
+            'item_number' => 'required|min:1|max:3',
+            'item_amount' => 'required|max:6',
+            'published' => 'required',
+        ]);
+        //バリデーション:エラー
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+    
+        //データ更新
+        $books = Book::find($request->id);
+        $books->item_name   = $request->item_name;
+        $books->item_number = $request->item_number;
+        $books->item_amount = $request->item_amount;
+        $books->published   = $request->published;
+        $books->save();
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -50,31 +50,7 @@ Route::post('/booksedit/{books}', function(Book $books) {
 });
 
 //更新処理
-Route::post('/books/update', function(Request $request){
-    //バリデーション
-        $validator = Validator::make($request->all(), [
-            'id' => 'required',
-            'item_name' => 'required|min:3|max:255',
-            'item_number' => 'required|min:1|max:3',
-            'item_amount' => 'required|max:6',
-            'published' => 'required',
-    ]);
-    //バリデーション:エラー
-        if ($validator->fails()) {
-            return redirect('/')
-                ->withInput()
-                ->withErrors($validator);
-    }
-    
-    //データ更新
-    $books = Book::find($request->id);
-    $books->item_name   = $request->item_name;
-    $books->item_number = $request->item_number;
-    $books->item_amount = $request->item_amount;
-    $books->published   = $request->published;
-    $books->save();
-    return redirect('/');
-});
+Route::post('/books/update','BooksController@update');
 
 /**
 * 本を削除 
